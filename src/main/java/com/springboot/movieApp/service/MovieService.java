cb38d44f309f0f87bbbf03bd5f4ceef6ce0a8706
Mullapudi <sai.g.mullapudi@accenture.com>
2023-11-06 16:38:29 +0530

    Adding service layer logic

---
+++ src/main/java/com/springboot/movieApp/service/MovieService.java
@@ -0,0 +1,18 @@
+package com.springboot.movieApp.service;
+
+import java.util.List;
+
+import com.springboot.movieApp.entity.Movie;
+
+public interface MovieService {
+
+	public Movie getMovieById(int movieId);
+	public List<Movie> getAllMovies();
+	public Movie saveMovieDetails(Movie movie);
+	public List<Movie> saveMultipleMovieDetails(List<Movie> movieList);
+	public Movie updateMovieDetails(Movie movie);
+	public boolean deleteMovie(int movieId);
+	
+	
+ 	
+}
